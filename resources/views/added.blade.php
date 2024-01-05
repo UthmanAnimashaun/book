ba1f481c48e4546cef607f4ce7c0b2237426b8f9
Uthman <animashaunua@gmail.com>
2024-01-05 23:11:52 +0100

    new added file added

---
+++ resources/views/added.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ut odit pariatur at! Libero, non? Fugit est accusantium numquam voluptates similique facere eligendi iusto assumenda minus aperiam nam culpa non corrupti sed quidem, harum amet sit voluptas, delectus necessitatibus, dolores odio. Pariatur repellat delectus aut quaerat quisquam blanditiis fuga atque quam commodi! Quia veniam eum alias iusto quo nihil sed dicta neque, corrupti repudiandae? Blanditiis nulla enim magnam dolore, distinctio officia ipsam eaque dignissimos provident repellat vel iste, excepturi nostrum tenetur voluptatum, esse cumque earum a. Rem eveniet modi deserunt ut! Accusamus debitis corporis assumenda suscipit officia numquam eligendi exercitationem.
+</body>
+</html>
